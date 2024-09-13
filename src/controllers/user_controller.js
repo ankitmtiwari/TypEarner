5f7e2bfb714356499ef3dcb4cba2adf74ec1262b
Ankit Tiwari <tankit291389@gmail.com>
2024-09-13 09:57:10 +0000

    login and registration ...

--- src/controllers/user_controller.js
+++ src/controllers/user_controller.js
@@ -1,43 +1,64 @@
-
-//Registration Controller
+// Get Registration Controller
 const registerUserController = async (req, res) => {
-    console.log("came to register view")
-    res.render("auth/registration");
-}
+  console.log("came to register view");
+  res.render("auth/registration");
+};
 
-//Registration Controller
+// Do Registration Controller
 const doRegisterUserController = async (req, res) => {
-    const {regEmail, regPassword, regConfirmPassword}= req.body;
-    console.log(regEmail, regPassword, regConfirmPassword)
-    if(regPassword === regConfirmPassword)
-    {
-        req.session.email=regEmail;
-        req.session.password=regConfirmPassword;
+  const {
+    regFirstName,
+    regLastName,
+    regEmail,
+    regPhone,
+    regPassword,
+    regConfirmPassword,
+  } = req.body;
 
-        res.redirect(301, 'login')
-    }
-    else{
-        res.render("auth/registration");
-    }
-}
+  //check if required fields are empty
+  if (
+    [regFirstName, regLastName, regEmail, regPhone, regPassword].some(
+      (field) => field?.trim() === undefined || field?.trim() === ""
+    )
+  ) {
+    return res.status(400).render( "auth/registration", {
+      success: false,
+      message:
+        "All Fields are required firstName,lastName, email, phoneNumber, password",
+    });
+  }
+  if (regPassword === regConfirmPassword) {
+    console.log(
+      regFirstName,
+      regLastName,
+      regEmail,
+      regPassword,
+      regConfirmPassword
+    );
+    req.session.email = regEmail;
 
-//Registration Controller
+    res.redirect(301, "login");
+  } else {
+    res.render("auth/registration", );
+  }
+};
+
+//Get Login Controller
 const loginUserController = async (req, res) => {
-    console.log("came to login")
-    const email= req.session.email;
-    const password=req.session.password;
+  console.log("came to login");
+  const email = req.session.email||'';
 
-    res.render("auth/login", {"email":email, "password":password});
-}
+  res.render("auth/login", { email: email});
+};
 
-const homePageController= async (req, res) => {
-    console.log("came to login")
-    res.render("task/index");
-}
+const homePageController = async (req, res) => {
+  console.log("came to login");
+  res.render("task/index");
+};
 
 export {
-    registerUserController,
-    doRegisterUserController,
-    loginUserController,
-    homePageController
+  registerUserController,
+  doRegisterUserController,
+  loginUserController,
+  homePageController,
 };
